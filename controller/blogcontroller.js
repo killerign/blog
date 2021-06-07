83f9e3db3498d292cbb3eea5cfc8cec6e8ec4ff8
killerign <srinivasananthu24@gmail.com>
2021-06-08 01:19:04 +0530

    version 2

--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -1,7 +1,7 @@
 const blog = require('../model/blog_model');
-
+var uuid = require('uuid');
 exports.getall= (req,res,next) => {
-    blog.find({},'heading summary picture tags topic date id author',{_id:0}).
+    blog.find({},'heading summary picture tags topic date uid author',{_id:0}).
     then(result => {
         next();
         console.log(result);
@@ -11,7 +11,7 @@ exports.getall= (req,res,next) => {
         res.sendStatus(400);
     })
 }
-/*
+
 exports.postone = (req,res,next) => {
     var today = new Date();
     var dd = today.getDate();
@@ -25,12 +25,12 @@ exports.postone = (req,res,next) => {
     today = yyyy+'/'+mm+'/'+dd;
     var obj = req.body;
     obj["cmpdate"]=today;
-    obj["id"]=uid();
+    obj["uid"]=uuid.v4();
     blog.insertMany(obj).then
-}*/
+}
 
 exports.getone = (req,res,next) => {
-    blog.findOne({"id": req.body.id}).   
+    blog.findOne({"uid": req.body.uid}).   
     then(result => {
         next();
         console.log(result);
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,9 +2,22 @@ const express = require('express');
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json();
 const router = express();
+const blog = require('../model/blog_model');
 const blogcontroller = require('../controller/blogcontroller')
 var cors = require('cors')
 router.use(cors());
 router.get('/getall',jsonParser,blogcontroller.getall);
 router.post('/getone',jsonParser,blogcontroller.getone);
+router.post('/postone',jsonParser,blogcontroller.postone);
+router.route('/uid/:uid').get((req, res) => {
+    blog.findOne({"uid": req.params.uid}).   
+    then(result => {
+        next();
+        console.log(result);
+        res.send(result);
+    })
+    .catch(err => {
+        res.sendStatus(400);
+    })
+});
 module.exports = router;
